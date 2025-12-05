33f5d5491673e4306045fc2c209905cbd90e252b
Nathan <melowise2001@yahoo.com>
2025-12-04 20:42:38 -0800

    updating static_root

--- backend/urls.py
+++ backend/urls.py
@@ -37,3 +37,4 @@ urlpatterns = [
 
 # tell django where to pull the images from in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
