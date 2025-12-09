cae82ec5b85078c54c51976397c8fcb521af94f1
Nathan <melowise2001@yahoo.com>
2025-12-08 18:31:59 -0800

    updating index.html trying to set the correct url to the index

--- backend/settings.py
+++ backend/settings.py
@@ -209,7 +209,7 @@ MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "frontend/build/static"
+    BASE_DIR / "backend/frontend/build/static"
     ]
 
 MEDIA_ROOT = "static/images"
