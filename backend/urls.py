ca328c378a431cce5df6d1de14febf91f2b70f3d
Nathan <melowise2001@yahoo.com>
2025-12-08 18:18:09 -0800

    updating index.html urlpath

--- backend/urls.py
+++ backend/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     
     path("admin/", admin.site.urls),
     #path("api/", include("base.urls")),
-    path("", TemplateView.as_view(template_name="index.html")),
+    #path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="../frontend/build/index.html")),
     path("api/products/", include("base.urls.product_urls")),
     path("api/users/", include("base.urls.user_urls")),
     path("api/orders/", include("base.urls.order_urls")),
